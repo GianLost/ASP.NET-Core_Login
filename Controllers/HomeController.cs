e3218c12d34789f83059983e1a4bd23ce3940231
GianLost <gianluca19993m@gmail.com>
2024-02-02 12:18:36 -0300

    Inicializando o repositório

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Core_Login.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult _LayoutLogin()
+    {
+        return Redirect("~/Shared/_LayoutLogin");
+    }
+}
