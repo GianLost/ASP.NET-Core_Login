c37e67e9b7e8f44ed24e75479ef3d32314273da6
GianLost <gianluca19993m@gmail.com>
2024-02-05 18:01:32 -0300

    Criação do formulário de registro de usuário

--- Migrations/20240205182104_Login_Migration-01.Designer.cs
+++ Migrations/20240205182104_Login_Migration-01.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ASP.NET_Core_Login.Migrations
 {
     [DbContext(typeof(LoginContext))]
-    [Migration("20240204222703_Login_Migration-01")]
+    [Migration("20240205182104_Login_Migration-01")]
     partial class Login_Migration01
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
